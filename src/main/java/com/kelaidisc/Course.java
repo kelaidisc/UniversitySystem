7f4df67b7549ea215d4bf78b21c7d94025ff1695
ChristoforosKelaidis <kelaidis.c@gmail.com>
2022-09-13 15:41:42 +0300

    first commit

---
+++ src/main/java/com/kelaidisc/Course.java
@@ -0,0 +1,23 @@
+package com.kelaidisc;
+
+public class Course {
+
+    private String name;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/kelaidisc/Student.java
@@ -0,0 +1,59 @@
+package com.kelaidisc;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phone;
+    private String birthday;
+    private int id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}

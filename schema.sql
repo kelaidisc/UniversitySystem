32ee846c1b4205bedeaa321c46206951b112db01
ChristoforosKelaidis <93557126+kelaidisc@users.noreply.github.com>
2022-09-13 15:45:39 +0300

    schema.sql file

---
+++ schema.sql
@@ -0,0 +1,3 @@
+CREATE TABLE professors(firstName TEXT, lastName TEXT, email TEXT, phone INTEGER, birthday TEXT, id INTEGER)
+CREATE TABLE students(firstName TEXT, lastName TEXT, email TEXT, phone INTEGER, birthday TEXT, id INTEGER)
+CREATE TABLE courses(name TEXT, id INTEGER)

abfcb83316666261fc5e9b3377b9d46f909e99f9
ChristoforosKelaidis <kelaidis.c@gmail.com>
2022-09-14 22:09:18 +0300

    set fields in professor class

--- src/main/java/com/kelaidisc/domain/Professor.java
+++ src/main/java/com/kelaidisc/domain/Professor.java
@@ -1,13 +1,16 @@
 package com.kelaidisc.domain;
 
+import java.time.LocalDate;
+
 public class Professor {
 
+    private int id;
     private String firstName;
     private String lastName;
     private String email;
-    private int phone;
-    private String birthday;
-    private int id;
+    private String phone;
+    private LocalDate birthday;
+
 
 
 }

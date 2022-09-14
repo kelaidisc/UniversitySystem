a317d1e67e05988fb490f9da243a0b1cab2035b9
ChristoforosKelaidis <kelaidis.c@gmail.com>
2022-09-14 22:06:58 +0300

    create table course statement

--- schema.sql
+++ schema.sql
@@ -20,4 +20,9 @@ CREATE TABLE `student` (
   unique (phone),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE courses(name TEXT, id INTEGER)
+CREATE TABLE `course` (
+  `id` int NOT NULL,
+  `name` varchar(60) NOT NULL,
+  unique (name),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

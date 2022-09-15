0b83c255419daa8f58a7bcc9c452df807a1bfa7a
ChristoforosKelaidis <kelaidis.c@gmail.com>
2022-09-15 15:57:37 +0300

    Added Select queries for course

--- queries.sql
+++ queries.sql
@@ -40,3 +40,53 @@ WHERE id=;
 DELETE FROM university.course
 WHERE id in();
 -----------------------------
+SELECT *
+FROM university.professor;
+-----------------------------
+SELECT *
+FROM university.professor where id=;
+-----------------------------
+SELECT *
+FROM university.professor where first_name like '%%';
+-----------------------------
+SELECT *
+FROM university.professor where last_name like '%%';
+-----------------------------
+SELECT *
+FROM university.professor where email='';
+-----------------------------
+SELECT *
+FROM university.professor where phone='';
+-----------------------------
+SELECT *
+FROM university.professor where birthday='';
+-----------------------------
+SELECT *
+FROM university.student;
+-----------------------------
+SELECT *
+FROM university.student where id=;
+-----------------------------
+SELECT *
+FROM university.student where first_name like '%%';
+-----------------------------
+SELECT *
+FROM university.student where last_name like '%%';
+-----------------------------
+SELECT *
+FROM university.student where email='';
+-----------------------------
+SELECT *
+FROM university.student where phone='';
+-----------------------------
+SELECT *
+FROM university.student where birthday='';
+-----------------------------
+SELECT *
+FROM university.course;
+-----------------------------
+SELECT *
+FROM university.course where id=;
+-----------------------------
+SELECT *
+FROM university.course where name like '%%';

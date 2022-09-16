e78d68b5117e5a125d982d8263f15261067588f4
ChristoforosKelaidis <kelaidis.c@gmail.com>
2022-09-16 15:48:54 +0300

    Added User abstract class

---
+++ src/main/java/com/kelaidisc/domain/User.java
@@ -0,0 +1,13 @@
+package com.kelaidisc.domain;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private LocalDate birthday;
+}

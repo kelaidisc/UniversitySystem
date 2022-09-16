195d9e9a14668fe8135f45d2d77c0d301cfe7fb6
ChristoforosKelaidis <kelaidis.c@gmail.com>
2022-09-16 15:49:37 +0300

    Changed Professor and Student to extend from User

--- src/main/java/com/kelaidisc/domain/Professor.java
+++ src/main/java/com/kelaidisc/domain/Professor.java
@@ -1,15 +1,8 @@
 package com.kelaidisc.domain;
 
-import java.time.LocalDate;
+public class Professor extends User{
 
-public class Professor {
 
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phone;
-    private LocalDate birthday;
 
 
 
--- src/main/java/com/kelaidisc/domain/Student.java
+++ src/main/java/com/kelaidisc/domain/Student.java
@@ -2,15 +2,6 @@ package com.kelaidisc.domain;
 
 import java.time.LocalDate;
 
-public class Student {
-
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phone;
-    private LocalDate birthday;
-
-
+public class Student extends User {
 
 }

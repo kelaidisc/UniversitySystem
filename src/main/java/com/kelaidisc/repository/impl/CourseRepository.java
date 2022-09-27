c907fba9fb425dda82d4b4b22505b074b36e7216
ChristoforosKelaidis <kelaidis.c@gmail.com>
2022-09-27 14:38:42 +0300

    Added CourseRepository.java interface

---
+++ src/main/java/com/kelaidisc/repository/impl/CourseRepository.java
@@ -0,0 +1,27 @@
+package com.kelaidisc.repository.impl;
+
+import com.kelaidisc.domain.Course;
+import com.kelaidisc.domain.Professor;
+import com.kelaidisc.domain.Student;
+
+import java.util.List;
+import java.util.Set;
+
+public interface CourseRepository {
+
+    List<Course> findAll();
+
+    List<Course> findAllByNameLike(String name); //despite being unique field,there might be chained Courses like Maths I, Maths II
+
+    Course findById(Long id);
+
+    Course create(Course course);
+
+    Course update(Course course);
+
+    void deleteByIds(Set<Long> ids);
+
+    Professor assignToCourse(Professor professor);
+
+    List<Student> enrollToCourse(List<Student> students);
+}

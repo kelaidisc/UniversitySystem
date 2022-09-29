427cb0b06cec6110121c34a7578de2aaf64237fb
ChristoforosKelaidis <kelaidis.c@gmail.com>
2022-09-29 14:29:14 +0300

    Removed @AllArgsConstructor from Professor.java

--- src/main/java/com/kelaidisc/domain/Professor.java
+++ src/main/java/com/kelaidisc/domain/Professor.java
@@ -1,6 +1,5 @@
 package com.kelaidisc.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -9,7 +8,6 @@ import lombok.experimental.SuperBuilder;
 @Data
 @SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Professor extends User {
 

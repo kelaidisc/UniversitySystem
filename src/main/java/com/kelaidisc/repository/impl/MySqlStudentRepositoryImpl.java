1a0dd3ea4bc467c7a8a87ab28cc416e6228c53c8
ChristoforosKelaidis <kelaidis.c@gmail.com>
2022-10-15 19:29:28 +0300

    Changed Crud Operations of MySqlStudentRepositoryImpl to operate with SqlUtils class

--- src/main/java/com/kelaidisc/repository/impl/MySqlStudentRepositoryImpl.java
+++ src/main/java/com/kelaidisc/repository/impl/MySqlStudentRepositoryImpl.java
@@ -1,10 +1,10 @@
 package com.kelaidisc.repository.impl;
 
+import static com.kelaidisc.common.SqlUtils.*;
 import static com.kelaidisc.shared.MySqlConnectionProvider.getInstance;
 
 import com.kelaidisc.domain.Student;
 import com.kelaidisc.repository.StudentRepository;
-
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
+import lombok.SneakyThrows;
 
 public class MySqlStudentRepositoryImpl implements StudentRepository {
 
@@ -39,25 +40,97 @@ public class MySqlStudentRepositoryImpl implements StudentRepository {
     private static final String DELETE_BY_IDS_Q = "DELETE FROM university.student\n" +
             "WHERE id in (?)";
 
-
-    public List<Student> getStudentsFromDatabase(String query, Consumer<PreparedStatement> consumer) {
-        List<Student> list = new ArrayList<>();
-        try (PreparedStatement ps = getInstance().getConn().prepareStatement(query)) {
-            consumer.accept(ps);
-            fillStudentList(list, ps);
-            return list;
-        } catch (SQLException e) {
-            System.out.println("Query failed " + e.getMessage());
-        }
-        return list;
+    @Override
+    public List<Student> findAll() {
+        return getStudentsFromDatabase(FIND_ALL_Q, (ps) -> {/*No Consumer arg*/});
     }
 
     @Override
-    public List<Student> findAll() {
-        return getStudentsFromDatabase(FIND_ALL_Q, (ps) -> {
-        });
+    public List<Student> findAllByFirstNameLike(String firstName) {
+        return getStudentsFromDatabase(FIND_ALL_BY_FIRST_NAME_Q, (ps) -> safeSetString(ps, 1, firstName));
     }
 
+    @Override
+    public List<Student> findAllByLastNameLike(String lastName) {
+       return getStudentsFromDatabase(FIND_ALL_BY_LAST_NAME_Q, (ps) -> safeSetString(ps, 1, lastName));
+    }
+
+    @Override
+    public List<Student> findAllByBirthday(LocalDate birthday) {
+       return getStudentsFromDatabase(FIND_ALL_BY_BIRTHDAY_Q, (ps) -> safeSetDate(ps, 1, Date.valueOf(birthday)));
+    }
+
+    @Override
+    public Student findById(Long id) {
+        return getStudentFromDatabase(FIND_BY_ID_Q, (ps) -> safeSetLong(ps, 1, id));
+    }
+
+
+    @Override
+    public Student findByEmail(String email) {
+        return getStudentFromDatabase(FIND_BY_EMAIL_Q, (ps) -> safeSetString(ps, 1, email));
+    }
+
+    @Override
+    public Student findByPhone(String phone) {
+        return getStudentFromDatabase(FIND_BY_PHONE_Q, (ps) -> safeSetString(ps, 1, phone));
+    }
+
+    @Override
+    @SneakyThrows
+    public Student create(Student student) {
+        try (PreparedStatement ps = getInstance().getConn().prepareStatement(CREATE_Q)) {
+            safeSetAllFields(student, ps);
+            safeExecuteUpdate(ps);
+            return student;
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    public Student update(Student student) {
+        try (PreparedStatement ps = getInstance().getConn().prepareStatement(UPDATE_Q)) {
+            safeSetAllFields(student, ps);
+            ps.setLong(6, student.getId());
+            safeExecuteUpdate(ps);
+            return student;
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    public void deleteByIds(Set<Long> ids) {
+        try (PreparedStatement ps = getInstance().getConn().prepareStatement(DELETE_BY_IDS_Q)) {
+            for (Long id : ids) {
+                ps.setLong(1, id);
+                safeExecuteUpdate(ps);
+            }
+        }
+    }
+
+    @SneakyThrows
+    public Student getStudentFromDatabase(String query,Consumer<PreparedStatement> consumer){
+        Student student = null;
+        try(PreparedStatement ps = getInstance().getConn().prepareStatement(query)){
+            consumer.accept(ps);
+            var rs = ps.executeQuery();
+            while (rs.next()){
+                student = convertResultToStudent(rs);
+            }
+            rs.close();
+            return student;
+        }
+    }
+
+    @SneakyThrows
+    private static void safeSetAllFields(Student student, PreparedStatement ps) {
+        ps.setString(1, student.getFirstName());
+        ps.setString(2, student.getLastName());
+        ps.setString(3, student.getEmail());
+        ps.setString(4, student.getPhone());
+        Date date = Date.valueOf(student.getBirthday());
+        ps.setDate(5, date);
+    }
     private void fillStudentList(List<Student> list, PreparedStatement ps) throws SQLException {
         var rs = ps.executeQuery();
         while (rs.next()) {
@@ -66,7 +139,17 @@ public class MySqlStudentRepositoryImpl implements StudentRepository {
         rs.close();
     }
 
-    private static Student convertResultToStudent(ResultSet rs) throws SQLException {
+    @SneakyThrows
+    private List<Student> getStudentsFromDatabase(String query, Consumer<PreparedStatement> consumer) {
+        List<Student> list = new ArrayList<>();
+        try (PreparedStatement ps = getInstance().getConn().prepareStatement(query)) {
+            consumer.accept(ps);
+            fillStudentList(list, ps);
+            return list;
+        }
+    }
+    @SneakyThrows
+    private Student convertResultToStudent(ResultSet rs) {
         Student student = new Student();
         student.setId(rs.getLong("id"));
         student.setFirstName(rs.getString("first_name"));
@@ -78,135 +161,5 @@ public class MySqlStudentRepositoryImpl implements StudentRepository {
         LocalDate dob = LocalDate.parse(date, formatter);
         student.setBirthday(dob);
         return student;
-    }
-
-    @Override
-    public List<Student> findAllByFirstNameLike(String firstName) {
-       return getStudentsFromDatabase(FIND_ALL_BY_FIRST_NAME_Q, (ps) -> {
-            try {
-                ps.setString(1, firstName);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    @Override
-    public List<Student> findAllByLastNameLike(String lastName) {
-       return getStudentsFromDatabase(FIND_ALL_BY_LAST_NAME_Q, (ps) -> {
-           try {
-               ps.setString(1, lastName);
-           } catch (SQLException e) {
-               throw new RuntimeException(e);
-           }
-       });
-    }
-
-    @Override
-    public List<Student> findAllByBirthday(LocalDate birthday) {
-       return getStudentsFromDatabase(FIND_ALL_BY_BIRTHDAY_Q, (ps) -> {
-           try  {
-               ps.setDate(1, java.sql.Date.valueOf(birthday));
-           } catch (SQLException e) {
-               System.out.println("Query failed " + e.getMessage());
-           }
-       });
-    }
-
-    public Student getStudentFromDatabase(String query,Consumer<PreparedStatement> consumer){
-        Student student = null;
-        try(PreparedStatement ps = getInstance().getConn().prepareStatement(query)){
-           consumer.accept(ps);
-           var rs = ps.executeQuery();
-           while (rs.next()){
-               student = convertResultToStudent(rs);
-           }
-           rs.close();
-           return student;
-        }catch (SQLException e){
-            System.out.println("Query Failed " + e.getMessage());
-        }
-        return student;
-    }
-
-    @Override
-    public Student findById(Long id) {
-        return getStudentFromDatabase(FIND_BY_ID_Q, (ps) -> {
-            try  {
-                ps.setLong(1, id);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-
-
-    @Override
-    public Student findByEmail(String email) {
-        return getStudentFromDatabase(FIND_BY_EMAIL_Q, (ps) ->{
-            try  {
-                ps.setString(1, email);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    @Override
-    public Student findByPhone(String phone) {
-        return getStudentFromDatabase(FIND_BY_PHONE_Q, (ps) -> {
-            try {
-                ps.setString(1, phone);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    @Override
-    public Student create(Student student) {
-        try (PreparedStatement ps = getInstance().getConn().prepareStatement(CREATE_Q)) {
-            psSetAllFields(student, ps);
-            ps.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.println("Creating student failed " + e.getMessage());
-        }
-        return student;
-    }
-
-    private static void psSetAllFields(Student student, PreparedStatement ps) throws SQLException {
-        ps.setString(1, student.getFirstName());
-        ps.setString(2, student.getLastName());
-        ps.setString(3, student.getEmail());
-        ps.setString(4, student.getPhone());
-        Date date = Date.valueOf(student.getBirthday());
-        ps.setDate(5, date);
-    }
-
-    @Override
-    public Student update(Student student) {
-        try (PreparedStatement ps = getInstance().getConn().prepareStatement(UPDATE_Q)) {
-            psSetAllFields(student, ps);
-            ps.setLong(6, student.getId());
-            ps.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.println("Update failed " + e.getMessage());
-        }
-        return student;
-    }
-
-    @Override
-    public void deleteByIds(Set<Long> ids) {
-        try (PreparedStatement ps = getInstance().getConn().prepareStatement(DELETE_BY_IDS_Q)) {
-            for (Long id : ids) {
-                ps.setLong(1, id);
-                ps.executeUpdate();
-            }
-        } catch (SQLException e) {
-            System.out.println("Delete failed " + e.getMessage());
-        }
     }
 }

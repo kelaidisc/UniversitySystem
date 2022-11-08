e053c55146869275d6597bf120296bc2024f13ca
Christoforos Kelaidis <kelaidis.c@gmail.com>
2022-11-08 12:56:57 +0200

    Added schema.sql and made *.sql files idempotent

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,35 +1,59 @@
-INSERT INTO university_spring.professor
-VALUES(1, '1984-07-08', 'johnnyg@gmail.com', 'John', 'Giannopoulos', '+30 6976770000'),
-      (2, '1983-09-10', 'andym@hotmail.com','Andy', 'Mazurcak', '+30 6999030405'),
-      (3, '1988-12-03', 'baziospan@gmail.com', 'Panos', 'Bazios', '+30 6979011011'),
-      (4, '1978-01-01',  'aiglig@gmail.com', 'Aglaia', 'Gioka', '+30 6923040404'),
-      (5, '1979-03-03', 'polifi@gmail.com', 'Ifigeneia', 'Polyzou', '+30 6914201420');
+INSERT IGNORE INTO university_spring.professor
+VALUES(1, 'John', 'Giannopoulos', 'johnnyg@gmail.com', '+30 6976770000', '1984-07-08');
+INSERT IGNORE INTO university_spring.professor
+VALUES(2,'Andy', 'Mazurcak', 'andym@hotmail.com', '+30 6999030405', '1983-09-10');
+INSERT IGNORE INTO university_spring.professor
+VALUES(3, 'Panos', 'Bazios', 'baziospan@gmail.com', '+30 6979011011', '1988-12-03');
+INSERT IGNORE INTO university_spring.professor
+VALUES(4, 'Aglaia', 'Gioka', 'aiglig@gmail.com', '+30 6923040404', '1978-01-01');
+INSERT IGNORE INTO university_spring.professor
+VALUES(5, 'Ifigeneia', 'Polyzou', 'polifi@gmail.com', '+30 6914201420', '1979-03-03');
 
-INSERT INTO university_spring.student
-VALUES(1, '1992-03-04', 'kefthom@gmail.com', 'Thomas', 'Kefalas', '+30 6994545556','2021-03-04'),
-      (2, '1997-06-12', 'alexkel12@hotmail.com', 'Alexandra', 'Kelaidi', '+30 6922324252','2022-03-04'),
-      (3, '1999-09-04', 'themipantz@hotmail.com', 'Themis', 'Pantzouris', '+30 6944454554','2020-01-01'),
-      (4, '2001-02-22', 'ioannisioan@gmail.com', 'Ioannis', 'Ioannou', '+30 6991911132','2019-05-05'),
-      (5, '2003-05-05', 'papa05mar@gmail.com', 'Maria', 'Papadopoulos', '+30 6922340599','2022-09-09');
+INSERT IGNORE INTO university_spring.student
+VALUES(1, 'Thomas', 'Kefalas', 'kefthom@gmail.com', '+30 6994545556', '1992-03-04','2021-03-04');
+INSERT IGNORE INTO university_spring.student
+VALUES(2, 'Alexandra', 'Kelaidi', 'alexkel12@hotmail.com', '+30 6922324252', '1997-06-12','2022-03-04');
+INSERT IGNORE INTO university_spring.student
+VALUES(3, 'Themis', 'Pantzouris', 'themipantz@hotmail.com', '+30 6944454554', '1999-09-04','2020-01-01');
+INSERT IGNORE INTO university_spring.student
+VALUES(4, 'Ioannis', 'Ioannou', 'ioannisioan@gmail.com', '+30 6991911132', '2001-02-22','2019-05-05');
+INSERT IGNORE INTO university_spring.student
+VALUES(5, 'Maria', 'Papadopoulos', 'papa05mar@gmail.com', '+30 6922340599', '2003-05-05','2022-09-09');
 
-INSERT INTO university_spring.course
-VALUES(1, 'Logistics', null),
-      (2, 'Microeconomics', null),
-      (3, 'Macroeconomics', null),
-      (4, 'Sociology', 1),
-      (5, 'Agricultural Economics', 2);
+INSERT IGNORE INTO university_spring.course
+VALUES(1, 'Logistics', null);
+INSERT IGNORE INTO university_spring.course
+VALUES(2, 'Microeconomics', null);
+INSERT IGNORE INTO university_spring.course
+VALUES(3, 'Macroeconomics', null);
+INSERT IGNORE INTO university_spring.course
+VALUES(4, 'Sociology', 1);
+INSERT IGNORE INTO university_spring.course
+VALUES(5, 'Agricultural Economics', 2);
 
-INSERT INTO university_spring.course_students
-VALUES(1, 1),
-      (1, 2),
-      (1, 3),
-      (2, 1),
-      (2, 2),
-      (2, 3),
-      (2, 4),
-      (3, 1),
-      (3, 2),
-      (3, 5),
-      (4, 1),
-      (4, 4),
-      (5, 5);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(1, 1);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(1, 2);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(1, 3);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(2, 1);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(2, 2);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(2, 3);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(2, 4);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(3, 1);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(3, 2);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(3, 5);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(4, 1);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(4, 4);
+INSERT IGNORE INTO university_spring.course_students
+VALUES(5, 5);

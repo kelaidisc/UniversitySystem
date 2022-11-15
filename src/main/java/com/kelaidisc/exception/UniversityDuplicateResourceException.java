4d4b1d5d472d100bde780f4133fab2c7ed3c3e70
Christoforos Kelaidis <kelaidis.c@gmail.com>
2022-11-15 17:15:27 +0200

    Updated UniversityDuplicateResourceException

--- src/main/java/com/kelaidisc/exception/UniversityDuplicateResourceException.java
+++ src/main/java/com/kelaidisc/exception/UniversityDuplicateResourceException.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class UniversityDuplicateResourceException extends RuntimeException {
     protected Object className;
-    protected String fieldName;
+    protected String name;
+    protected String fieldValue;
 
-    public UniversityDuplicateResourceException(Object className, String fieldName) {
+    public UniversityDuplicateResourceException(Object className, String name, String fieldValue) {
 
-        super(String.format("%s: %s already exists", className.getClass().getName(), fieldName));
+        super(String.format("%s with %s: %s already exists", className.getClass().getName(), name, fieldValue));
         this.className = className;
-        this.fieldName = fieldName;
+        this.name = name;
+        this.fieldValue = fieldValue;
     }
 
 }

261b01ff0dd53552a61067597a84977cd6d70b45
Christoforos Kelaidis <kelaidis.c@gmail.com>
2023-01-02 14:52:39 +0200

    Added custom exceptions for validation in StudentController

--- src/main/java/com/kelaidisc/controller/StudentController.java
+++ src/main/java/com/kelaidisc/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.kelaidisc.controller;
 
+import static java.util.Objects.nonNull;
+
 import com.kelaidisc.domain.Course;
 import com.kelaidisc.domain.Student;
 import com.kelaidisc.dto.DeleteDto;
@@ -7,15 +9,18 @@ import com.kelaidisc.dto.student.StudentCreateDto;
 import com.kelaidisc.dto.student.StudentUpdateDto;
 import com.kelaidisc.exception.UniversityBadRequestException;
 import com.kelaidisc.service.StudentService;
+import java.security.InvalidParameterException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,9 +45,15 @@ public class StudentController {
 
   @GetMapping
   public List<Student> findAll(@RequestParam(value = "name", required = false) String name,
-                               @RequestParam(value = "email", required = false) @Email String email,
+                               @RequestParam(value = "email", required = false) String email,
                                @RequestParam(value = "phone", required = false) String phone,
-                               @RequestParam(value = "birthday", required = false) LocalDate birthday) {
+                               @RequestParam(value = "birthday", required = false)
+                                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                 LocalDate birthday) {
+
+    validateName(name);
+    validateEmail(email);
+    validatePhone(phone);
 
     return studentService.search(name, email, phone, birthday);
   }
@@ -81,5 +92,45 @@ public class StudentController {
         .toList();
   }
 
+  private void validateName(String name) {
 
+    if (nonNull(name) && name.trim().length() == 0) {
+      throw new InvalidParameterException("Name can't be empty");
+    }
+  }
+
+  private void validateEmail(String email) {
+
+    if (nonNull(email) && email.trim().length() == 0) {
+      throw new InvalidParameterException("Email can't be empty");
+    }
+
+    String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+    Pattern pattern = Pattern.compile(regex);
+
+    if (nonNull(email)) {
+
+      Matcher matcher = pattern.matcher(email);
+      if (!matcher.matches()) {
+        throw new InvalidParameterException("Invalid email");
+      }
+    }
+  }
+
+  private void validatePhone(String phone) {
+
+    if (nonNull(phone) && phone.trim().length() == 0) {
+      throw new InvalidParameterException("Phone can't be empty");
+    }
+
+    String regex = "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$";
+    Pattern pattern = Pattern.compile(regex);
+
+    if (nonNull(phone)) {
+      Matcher matcher = pattern.matcher(phone);
+      if (!matcher.matches()) {
+        throw new InvalidParameterException("Invalid phone");
+      }
+    }
+  }
 }
--- src/main/java/com/kelaidisc/exception/UniversityExceptionHandler.java
+++ src/main/java/com/kelaidisc/exception/UniversityExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.kelaidisc.exception;
 
+import java.security.InvalidParameterException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import javax.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -26,4 +29,19 @@ public class UniversityExceptionHandler extends ResponseEntityExceptionHandler {
     );
     return new ResponseEntity<>(errorDetails, httpStatus);
   }
+
+  @ExceptionHandler ({ConstraintViolationException.class})
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  protected ResponseEntity<Object> handleConstraintViolationException(
+      ConstraintViolationException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler ({InvalidParameterException.class})
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  protected ResponseEntity<Object> handleInvalidParameterException(
+      InvalidParameterException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+
 }

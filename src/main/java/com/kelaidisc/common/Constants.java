ffefd7afcf9590cb70ff9b4ac802013b62ae5b65
Christoforos Kelaidis <kelaidis.c@gmail.com>
2023-01-04 15:01:42 +0200

    cleanup

--- src/main/java/com/kelaidisc/common/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.kelaidisc.common;
-
-import java.time.format.DateTimeFormatter;
-
-public class Constants {
-
-  public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-}
--- src/main/java/com/kelaidisc/repository/UserRepository.java
+++ src/main/java/com/kelaidisc/repository/UserRepository.java
@@ -1,27 +1,12 @@
 package com.kelaidisc.repository;
 
 import com.kelaidisc.domain.User;
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.query.Param;
 
 @NoRepositoryBean
 public interface UserRepository<T extends User> extends CrudRepository<T, Long> {
-
-  List<T> findAll();
-
-  List<T> findAllByFirstNameContainingIgnoreCase(@Param("firstName") String firstName);
-
-  List<T> findAllByLastNameContainingIgnoreCase(@Param("lastName") String lastName);
-
-  List<T> findAllByBirthday(@Param("birthday") LocalDate birthday);
-
-  T findByEmail(@Param("email") String email);
-
-  T findByPhone(@Param("phone") String phone);
 
   void deleteAllByIdIn(Set<Long> ids);
 
